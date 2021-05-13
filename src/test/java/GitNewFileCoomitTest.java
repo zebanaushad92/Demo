99079ed09646fb25dbbf76d76ac9e13ce6f86070
Zeba Naushad <zebanaushad@Zebas-MacBook-Air.local>
2021-05-13 17:19:30 +0530

    gittrial

---
+++ src/test/java/GitNewFileCoomitTest.java
@@ -0,0 +1,6 @@
+
+public class GitNewFileCoomitTest {
+	
+	
+
+}
--- src/test/java/VerboseTest.java
+++ src/test/java/VerboseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+//Author Zeba
 public class VerboseTest {
 	 WebDriver driver;
   
